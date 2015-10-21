3253240ff585d8a833eafcbea394a307219c6e37
Mohammad Kalim <mohamamdkalim@mohamamds-macbook-pro.local>
2015-10-21 18:52:28 +0500

    Initial Commit

---
+++ demoAnimated/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoAnimated
+//
+//  Created by Mohamamd Kalim on 10/21/15.
+//  Copyright © 2015 Mohammad Kalim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demoAnimated/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demoAnimated
+//
+//  Created by Mohamamd Kalim on 10/21/15.
+//  Copyright © 2015 Mohammad Kalim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
